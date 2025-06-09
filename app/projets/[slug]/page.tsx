1783d95591dfe0d8429ac324d25ef9c9b1fb7d65
Noxatris <81755314+Noxatris@users.noreply.github.com>
2025-06-09 19:43:06 -0400

    Refactor generateStaticParams for improved type safety and update ProjectPage to use await for project retrieval

--- app/projets/[slug]/page.tsx
+++ app/projets/[slug]/page.tsx
@@ -2,18 +2,12 @@ import { notFound } from 'next/navigation';
 import ProjectTemplate from '../../(components)/ProjectTemplate';
 import projects from '../../../data/projets.json';
 
-export async function generateStaticParams(): Promise<{ params: { slug: string } }[]> {
-  return projects.map(project => ({
-    params: { slug: project.slug },
-  }));
+export async function generateStaticParams() {
+  return projects.map(project => ({ slug: project.slug }));
 }
 
-export default async function ProjectPage({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const project = projects.find(p => p.slug === params.slug);
+export default async function ProjectPage({ params }: { params: { slug: string } }) {
+  const project = await projects.find(p => p.slug === params.slug);
 
   if (!project) return notFound();
 
