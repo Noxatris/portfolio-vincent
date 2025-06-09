6d61c7524570186cd17570a824fb460b1509e80c
Noxatris <81755314+Noxatris@users.noreply.github.com>
2025-06-09 19:40:41 -0400

    Refactor generateStaticParams to improve type safety in parameter structure

--- app/projets/[slug]/page.tsx
+++ app/projets/[slug]/page.tsx
@@ -2,10 +2,12 @@ import { notFound } from 'next/navigation';
 import ProjectTemplate from '../../(components)/ProjectTemplate';
 import projects from '../../../data/projets.json';
 
-export async function generateStaticParams(): Promise<{ slug: string }[]> {
-  return projects.map(project => ({ slug: project.slug }));
+export async function generateStaticParams(): Promise<{ params: { slug: string } }[]> {
+  return projects.map(project => ({
+    params: { slug: project.slug },
+  }));
 }
-// eslint-disable-next-line
+
 export default async function ProjectPage({
   params,
 }: {
