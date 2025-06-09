81342daa199b8e4dd516bb5d78705b1a08ccbfb9
Noxatris <81755314+Noxatris@users.noreply.github.com>
2025-06-09 19:38:40 -0400

    Refactor generateStaticParams and ProjectPage function signatures for improved type safety and readability

--- app/projets/[slug]/page.tsx
+++ app/projets/[slug]/page.tsx
@@ -2,11 +2,15 @@ import { notFound } from 'next/navigation';
 import ProjectTemplate from '../../(components)/ProjectTemplate';
 import projects from '../../../data/projets.json';
 
-export async function generateStaticParams() {
+export async function generateStaticParams(): Promise<{ slug: string }[]> {
   return projects.map(project => ({ slug: project.slug }));
 }
 // eslint-disable-next-line
-export default async function ProjectPage({ params }: { params: { slug: string } }) {
+export default async function ProjectPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
   const project = projects.find(p => p.slug === params.slug);
 
   if (!project) return notFound();
